a1a37eb2ca7d3b8dc0280dd09e157671cad948cf
失った <168133165+yophlox@users.noreply.github.com>
2024-12-26 08:01:19 -0500

    hmm

---
+++ Assets/PhloxUtil/GameOBJReset.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GameOBJReset : MonoBehaviour
+{
+    [SerializeField] private GameObject[] managedObjects;
+    
+    private void Start()
+    {
+        EnableAll();
+        
+        DisableAll();
+    }
+    
+    public void EnableAll()
+    {
+        foreach (GameObject obj in managedObjects)
+        {
+            if (obj != null)
+                obj.SetActive(true);
+        }
+    }
+
+    public void DisableAll()
+    {
+        foreach (GameObject obj in managedObjects)
+        {
+            if (obj != null)
+                obj.SetActive(false);
+        }
+    }
+}
